3403ddab63129bca7ef05023ccedc8b0cc048252
Travis Jones <travismartinjones@gmail.com>
2020-10-22 08:52:16 -0500

    Add installation of dot net into install script

--- install.sh
+++ install.sh
@@ -37,6 +37,15 @@ echo "Install DBUS service"
 sudo cp /home/pi/moonboard/ble/com.moonboard.conf /etc/dbus-1/system.d
 sudo cp /home/pi/moonboard/ble/com.moonboard.service /usr/share/dbus-1/system-services/
 
+echo "Install .Net Core"
+wget https://download.visualstudio.microsoft.com/download/pr/349f13f0-400e-476c-ba10-fe284b35b932/44a5863469051c5cf103129f1423ddb8/dotnet-sdk-3.1.102-linux-arm.tar.gz
+wget https://download.visualstudio.microsoft.com/download/pr/8ccacf09-e5eb-481b-a407-2398b08ac6ac/1cef921566cb9d1ca8c742c9c26a521c/aspnetcore-runtime-3.1.2-linux-arm.tar.gz
+sudo mkdir -p /opt/dotnet
+sudo tar zxf dotnet-sdk-3.1.102-linux-arm.tar.gz -C /opt/dotnet
+sudo tar zxf aspnetcore-runtime-3.1.2-linux-arm.tar.gz -C /opt/dotnet
+export DOTNET_ROOT=/opt/dotnet
+export PATH=/opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
 
 echo "Prepare logfiles"
 sudo touch /var/log/moonboard

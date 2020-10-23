3256395a1fe4c28d64d6b63fa663b19572b41b92
Travis Jones <travismartinjones@gmail.com>
2020-10-23 05:04:59 +0100

    Add DBus interface.

---
+++ frontend/dbus/moonboard.DBus.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Tmds.DBus;
+
+[assembly: InternalsVisibleTo(Tmds.DBus.Connection.DynamicAssemblyName)]
+namespace moonboard.DBus
+{
+    [DBusInterface("com.moonboard")]
+    interface IMoonboard : IDBusObject
+    {
+        Task<IDisposable> Watchnew_problemAsync(Action<string> handler, Action<Exception> onError = null);
+    }
+
+    [DBusInterface("org.freedesktop.DBus.ObjectManager")]
+    interface IObjectManager : IDBusObject
+    {
+        Task<IDictionary<ObjectPath, IDictionary<string, IDictionary<string, object>>>> GetManagedObjectsAsync();
+    }
+
+    [DBusInterface("org.bluez.GattCharacteristic1")]
+    interface IGattCharacteristic1 : IDBusObject
+    {
+        Task<byte[]> ReadValueAsync(IDictionary<string, object> Options);
+        Task WriteValueAsync(byte[] Value, IDictionary<string, object> Options);
+        Task StartNotifyAsync();
+        Task StopNotifyAsync();
+    }
+}

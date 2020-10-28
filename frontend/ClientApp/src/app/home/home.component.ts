9e613eff468e753e94d775e1b2f87a0375de0463
Travis Jones <travis@dauberapp.com>
2020-10-28 16:58:14 -0500

    Front end work in progress.

--- frontend/ClientApp/src/app/home/home.component.ts
+++ frontend/ClientApp/src/app/home/home.component.ts
@@ -8,6 +8,7 @@ class Cell {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   cells: Cell[][];
--- frontend/ClientApp/src/app/problem/problem.component.ts
+++ frontend/ClientApp/src/app/problem/problem.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'problem-component',
-  templateUrl: './problem.component.html'
+  templateUrl: './problem.component.html',
+  styleUrls: ['./problem.component.css']
 })
 export class ProblemComponent {
 
